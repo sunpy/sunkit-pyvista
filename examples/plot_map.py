a337942dc43c63c697f59ffc968cdac8692ed1bf
jeffreypaul15 <jeffrey.paul2000@gmail.com>
2021-05-27 17:58:07 +0530

    added xvfbwrapper

--- examples/plot_map.py
+++ examples/plot_map.py
@@ -8,6 +8,10 @@ This example shows how to plot map in 3D.
 from sunpy.data.sample import AIA_193_IMAGE
 from sunpy.map import Map
 from sunkit_pyvista import SunpyPlotter
+from xvfbwrapper import Xvfb
+
+vdisplay = Xvfb()
+vdisplay.start()
 
 m = Map(AIA_193_IMAGE)
 m.plot()
@@ -16,4 +20,5 @@ plotter = SunpyPlotter()
 map_mesh = plotter.plot_map(m)
 line_mesh = plotter.plot_solar_axis()
 
+vdisplay.stop()
 # plotter.show(cpos=(-100,0,0))

8c03cebe487c459139b6e83dcf61e6773059fb6c
jeffreypaul15 <jeffrey.paul2000@gmail.com>
2021-05-27 10:05:07 +0530

    tests should work now

--- sunkit_pyvista/plotter.py
+++ sunkit_pyvista/plotter.py
@@ -137,7 +137,7 @@ class SunpyPlotter:
 from sunpy.data.sample import AIA_335_IMAGE
 from sunpy.map import Map
 
-pv.start_xvfb()
+# pv.start_xvfb()
 m = Map(AIA_335_IMAGE)
 m.plot()
 plotter = SunpyPlotter()

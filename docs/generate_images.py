d76cc3928f8d0de61fc2d5ee78fc79266319219d
jeffreypaul15 <jeffrey.paul2000@gmail.com>
2021-05-29 01:16:04 +0530

    Test if generating scripts works

--- docs/generate_images.py
+++ docs/generate_images.py
@@ -1,4 +1,7 @@
 import os
-for script in os.listdir('../examples'):
+
+ROOT = os.path.join(os.path.dirname(__file__), '../')
+
+for script in os.listdir(os.path.join(ROOT, 'examples')):
     if script.endswith('py'):
         os.system(f"python ../examples/{script}")
--- sunkit_pyvista/version.py
+++ sunkit_pyvista/version.py
@@ -5,4 +5,4 @@ try:
     from setuptools_scm import get_version
     __version__ = get_version(root='..', relative_to=__file__)
 except Exception:
-    __version__ = '0.1.dev26+geb005c9.d20210528'
+    __version__ = '0.1.dev27+g9ad2d4f.d20210528'

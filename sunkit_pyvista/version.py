c5c6b02639596a19f5de3279d0d9b8b07195c8d0
jeffreypaul15 <jeffrey.paul2000@gmail.com>
2021-05-29 15:33:26 +0530

    revert to original code

--- sunkit_pyvista/version.py
+++ sunkit_pyvista/version.py
@@ -5,4 +5,4 @@ try:
     from setuptools_scm import get_version
     __version__ = get_version(root='..', relative_to=__file__)
 except Exception:
-    __version__ = '0.1.dev17+g62c48cf.d20210529'
+    __version__ = '0.1.dev20+g6d456a5.d20210529'

1ede4dcb02a281f2c0b7f11d17330b97ec32d5b5
jeffreypaul15 <jeffrey.paul2000@gmail.com>
2021-06-29 20:57:16 +0530

    changes to the right example file

--- examples/skip_plot_map_in_3d.py
+++ examples/skip_plot_map_in_3d.py
@@ -1,48 +1,58 @@
 """
-===========================
-Finding the brightest pixel
-===========================
+=======================================
+Three dimensional plots with sunpy Maps
+=======================================
 
-sunkit-pyvista can be used to overplot the location of pixels
-on the map.
+Using sunkit-pyvista, one can interface with the `pyvista` package to
+produce interactive 3D plots for sunpy Maps.
 """
 
-import numpy as np
-
+import astropy.constants as const
 import astropy.units as u
-from astropy.constants import R_sun
 from astropy.coordinates import SkyCoord
 from sunpy.coordinates import frames
-from sunpy.data.sample import AIA_171_IMAGE
+from sunpy.data.sample import AIA_193_IMAGE
 from sunpy.map import Map
 
 from sunkit_pyvista import SunpyPlotter
 
 ###############################################################################
-# We will firstly use an AIA 171 image from the sunpy sample data as the base image.
-m = Map(AIA_171_IMAGE)
+# We will firstly use an AIA 193 image from the sunpy sample data as the base image.
+m = Map(AIA_193_IMAGE)
+
+###############################################################################
+# 3D plots are done on "plotter" objects, which are similar to matplotlib axes.
+# sunkit-pyvista has a built in `SunpyPlotter` class that can be used to plot maps
+# and coordinate aware objects.
 
 # Start by creating a plotter
 plotter = SunpyPlotter()
-
 # Plot a map
-plotter.plot_map(m, clip_interval=(1, 99)*u.percent)
+plotter.plot_map(m)
 # Add an arrow to show the solar rotation axis
 plotter.plot_solar_axis()
+# Plot an arbitrary line
+line = SkyCoord(lon=[180, 190, 200] * u.deg,
+                lat=[0, 10, 20] * u.deg,
+                distance=[1, 2, 3] * const.R_sun,
+                frame='heliocentricinertial')
+plotter.plot_coordinates(line)
 
-# To find the brightest pixel, we find the maximum in the AIA image data then
-# transform that pixel coordinate to a map coordinate.
-pixel_pos = np.argwhere(m.data == m.data.max()) * u.pixel
-hpc_max = m.pixel_to_world(pixel_pos[:, 1], pixel_pos[:, 0])
+# Plot an arbitrary point
+point = SkyCoord(lon=30 * u.deg,
+                 lat=-10 * u.deg,
+                 obstime=m.date,
+                 frame=frames.HeliographicStonyhurst)
+plotter.plot_coordinates(point, color='yellow')
 
-# To plot a overplot particular coordinate, we simply
-# use the :meth:`~sunkit_pyvista.plotter.SunpyPlotter.plot_coordinates`
-# with a single coordinate as an argument.
-plotter.plot_coordinates(hpc_max, radius=0.04, color='blue')
+plotter.set_camera_coordinate(m.observer_coordinate)
 
-# Setting the camera coordinate to view the point.
-camera_position = SkyCoord(30*u.deg, -10*u.deg, 6*R_sun,
-                           frame=frames.HeliographicStonyhurst,
-                           obstime=m.date)
-plotter.set_camera_coordinate(camera_position)
+# Set the view angle of the plot
+plotter.set_view_angle(m.rsun_obs)
+
+# Plot a quadrangle with width of 20 degrees and a height of 60 degrees
+bottom_left = SkyCoord(30*u.deg, -10*u.deg,
+                       frame=frames.HeliographicStonyhurst,
+                       obstime=m.date)
+plotter.plot_quadrangle(bottom_left=bottom_left, width=20*u.deg, height=60*u.deg, color='blue')
 plotter.show()

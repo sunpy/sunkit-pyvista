0615cedfc404e50a86839077e42911a3ac194ff1
jeffreypaul15 <jeffrey.paul2000@gmail.com>
2021-07-02 01:44:15 +0530

    Updates 3rd example

--- examples/skip_plot_functionality.py
+++ examples/skip_plot_functionality.py
@@ -7,7 +7,8 @@ sunkit-pyvista also extends features from `sunpy`, producing
 them in 3D.
 """
 
-import astropy.constants as const
+import numpy as np
+
 import astropy.units as u
 from astropy.constants import R_sun
 from astropy.coordinates import SkyCoord
@@ -24,17 +25,16 @@ m = Map(AIA_171_IMAGE)
 # Start by creating a plotter
 plotter = SunpyPlotter()
 # Plot a map
-plotter.plot_map(m)
+plotter.plot_map(m, clip_interval=(1, 99.9)*u.percent)
 # Add an arrow to show the solar rotation axis
 plotter.plot_solar_axis()
 
 # We can also plot an arbitrary point by passing a single coordinate to
 # :meth:`~sunkit_pyvista.plotter.SunpyPlotter.plot_coordinates`.
-point = SkyCoord(lon=30 * u.deg,
-                 lat=-10 * u.deg,
-                 obstime=m.date,
-                 frame=frames.HeliographicStonyhurst)
-plotter.plot_coordinates(point, color='blue')
+# Plotting a point on the brightest pixel in the map
+pixel_pos = np.argwhere(m.data == m.data.max()) * u.pixel
+hpc_max = m.pixel_to_world(pixel_pos[:, 1], pixel_pos[:, 0])
+plotter.plot_coordinates(hpc_max, color='blue')
 
 # Plot a quadrangle with width of 20 degrees and a height of 60 degrees
 bottom_left = SkyCoord(30*u.deg, -10*u.deg,
@@ -43,9 +43,9 @@ bottom_left = SkyCoord(30*u.deg, -10*u.deg,
 plotter.plot_quadrangle(bottom_left=bottom_left, width=20*u.deg,
                         height=60*u.deg, color='blue')
 
-# Set the camera coordinate
-bottom_left = SkyCoord(30*u.deg, -10*u.deg, 7*R_sun,
-                       frame=frames.HeliographicStonyhurst,
-                       obstime=m.date)
-plotter.set_camera_coordinate(bottom_left)
+# Set the camera coordinate to view the plot correctly
+camera_coord = SkyCoord(30*u.deg, -10*u.deg, 6*R_sun,
+                        frame=frames.HeliographicStonyhurst,
+                        obstime=m.date)
+plotter.set_camera_coordinate(camera_coord)
 plotter.show()

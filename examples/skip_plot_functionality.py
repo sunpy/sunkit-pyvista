49fd0c2bad6a96c3f380cede0254df617618f6b1
jeffreypaul15 <jeffrey.paul2000@gmail.com>
2021-07-02 01:34:57 +0530

    Updates examples

--- examples/skip_plot_functionality.py
+++ examples/skip_plot_functionality.py
@@ -1,10 +1,10 @@
 """
 =======================================
-Three dimensional plots with sunpy Maps
+Three  with sunpy Maps
 =======================================
 
-Using sunkit-pyvista, one can interface with the `pyvista` package to
-produce interactive 3D plots for sunpy Maps.
+sunkit-pyvista also extends features from `sunpy`, producing
+them in 3D.
 """
 
 import astropy.constants as const
@@ -12,19 +12,14 @@ import astropy.units as u
 from astropy.constants import R_sun
 from astropy.coordinates import SkyCoord
 from sunpy.coordinates import frames
-from sunpy.data.sample import AIA_193_IMAGE
+from sunpy.data.sample import AIA_171_IMAGE
 from sunpy.map import Map
 
 from sunkit_pyvista import SunpyPlotter
 
 ###############################################################################
-# We will firstly use an AIA 193 image from the sunpy sample data as the base image.
-m = Map(AIA_193_IMAGE)
-
-###############################################################################
-# 3D plots are done on "plotter" objects, which are similar to matplotlib axes.
-# sunkit-pyvista has a built in `SunpyPlotter` class that can be used to plot maps
-# and coordinate aware objects.
+# We will firstly use an AIA 171 image from the sunpy sample data as the base image.
+m = Map(AIA_171_IMAGE)
 
 # Start by creating a plotter
 plotter = SunpyPlotter()
@@ -32,19 +27,21 @@ plotter = SunpyPlotter()
 plotter.plot_map(m)
 # Add an arrow to show the solar rotation axis
 plotter.plot_solar_axis()
-# Plot an arbitrary line
-line = SkyCoord(lon=[180, 190, 200] * u.deg,
-                lat=[0, 10, 20] * u.deg,
-                distance=[1, 2, 3] * const.R_sun,
-                frame='heliocentricinertial')
-plotter.plot_coordinates(line)
 
-# Plot an arbitrary point
+# We can also plot an arbitrary point by passing a single coordinate to
+# :meth:`~sunkit_pyvista.plotter.SunpyPlotter.plot_coordinates`.
 point = SkyCoord(lon=30 * u.deg,
                  lat=-10 * u.deg,
                  obstime=m.date,
                  frame=frames.HeliographicStonyhurst)
 plotter.plot_coordinates(point, color='blue')
+
+# Plot a quadrangle with width of 20 degrees and a height of 60 degrees
+bottom_left = SkyCoord(30*u.deg, -10*u.deg,
+                       frame=frames.HeliographicStonyhurst,
+                       obstime=m.date)
+plotter.plot_quadrangle(bottom_left=bottom_left, width=20*u.deg,
+                        height=60*u.deg, color='blue')
 
 # Set the camera coordinate
 bottom_left = SkyCoord(30*u.deg, -10*u.deg, 7*R_sun,

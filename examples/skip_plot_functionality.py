d50dc810733b34810087cde1ae0af8d2b1e7a200
Jeffrey Aaron Paul <50923538+jeffreypaul15@users.noreply.github.com>
2021-07-04 07:29:54 +0000

    Rearranged examples with the current functionality (#30)
    Co-authored-by: Nabil Freij <nabil.freij@gmail.com>

---
+++ examples/skip_plot_functionality.py
@@ -0,0 +1,52 @@
+"""
+==================================
+Extending functionality from sunpy
+==================================
+
+sunkit-pyvista also extends :meth:`~sunpy.map.GenericMap.draw_quadrangle` from `sunpy` and :meth:`~astropy.visualization.wcsaxes.WCSAxes.plot_coord` from `astropy` to produce them in 3D.
+"""
+
+import numpy as np
+
+import astropy.units as u
+from astropy.constants import R_sun
+from astropy.coordinates import SkyCoord
+from sunpy.coordinates import frames
+from sunpy.data.sample import AIA_171_IMAGE
+from sunpy.map import Map
+
+from sunkit_pyvista import SunpyPlotter
+
+###############################################################################
+# We will firstly use an AIA 171 image from the sunpy sample data as the base image.
+m = Map(AIA_171_IMAGE)
+
+###############################################################################
+# Start by creating a plotter
+plotter = SunpyPlotter()
+# Plot a map
+plotter.plot_map(m, clip_interval=(1, 99.9)*u.percent)
+# Add an arrow to show the solar rotation axis
+plotter.plot_solar_axis()
+
+###############################################################################
+# We can also plot an arbitrary point by passing a single coordinate to
+# :meth:`~sunkit_pyvista.plotter.SunpyPlotter.plot_coordinates`.
+# Plotting a point on the brightest pixel in the map
+pixel_pos = np.argwhere(m.data == m.data.max()) * u.pixel
+hpc_max = m.pixel_to_world(pixel_pos[:, 1], pixel_pos[:, 0])
+plotter.plot_coordinates(hpc_max, color='blue')
+
+# Plot a quadrangle with width of 20 degrees and a height of 60 degrees
+bottom_left = SkyCoord(30*u.deg, -10*u.deg,
+                       frame=frames.HeliographicStonyhurst,
+                       obstime=m.date)
+plotter.plot_quadrangle(bottom_left=bottom_left, width=20*u.deg,
+                        height=60*u.deg, color='blue')
+
+# Set the camera coordinate to view the plot correctly
+camera_coord = SkyCoord(30*u.deg, -10*u.deg, 6*R_sun,
+                        frame=frames.HeliographicStonyhurst,
+                        obstime=m.date)
+plotter.set_camera_coordinate(camera_coord)
+plotter.show()
--- examples/skip_plot_map_in_3d.py
+++ examples/skip_plot_map_in_3d.py
@@ -10,7 +10,6 @@ produce interactive 3D plots for sunpy Maps.
 import astropy.constants as const
 import astropy.units as u
 from astropy.coordinates import SkyCoord
-from sunpy.coordinates import frames
 from sunpy.data.sample import AIA_193_IMAGE
 from sunpy.map import Map
 
@@ -27,32 +26,19 @@ m = Map(AIA_193_IMAGE)
 
 # Start by creating a plotter
 plotter = SunpyPlotter()
-# Plot a map
+
+# Plot a map by specifying a clip interval to clip data
+# according to the percentile interval specified.
 plotter.plot_map(m, clip_interval=(0, 99)*u.percent)
+
 # Add an arrow to show the solar rotation axis
 plotter.plot_solar_axis()
+
 # Plot an arbitrary line
 line = SkyCoord(lon=[180, 190, 200] * u.deg,
                 lat=[0, 10, 20] * u.deg,
                 distance=[1, 2, 3] * const.R_sun,
                 frame='heliocentricinertial')
+
 plotter.plot_coordinates(line)
-
-# Plot an arbitrary point
-point = SkyCoord(lon=30 * u.deg,
-                 lat=-10 * u.deg,
-                 obstime=m.date,
-                 frame=frames.HeliographicStonyhurst)
-plotter.plot_coordinates(point, color='yellow')
-
-plotter.set_camera_coordinate(m.observer_coordinate)
-
-# Set the view angle of the plot
-plotter.set_view_angle(m.rsun_obs)
-
-# Plot a quadrangle with width of 20 degrees and a height of 60 degrees
-bottom_left = SkyCoord(30*u.deg, -10*u.deg,
-                       frame=frames.HeliographicStonyhurst,
-                       obstime=m.date)
-plotter.plot_quadrangle(bottom_left=bottom_left, width=20*u.deg, height=60*u.deg, color='blue')
 plotter.show()

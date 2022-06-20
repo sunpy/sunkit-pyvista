30f14cf0944a14045872d1707aa59e0dd56c7e0b
Nabil Freij <nabil.freij@gmail.com>
2022-06-20 16:09:48 -0700

    add another example

--- examples/3d_functionality.py
+++ examples/3d_functionality.py
@@ -18,6 +18,7 @@ from sunkit_pyvista.sample import low_res_aia_193
 
 ###############################################################################
 # We will use an AIA 193 image from the sunpy sample data as the base image.
+
 # Start by creating a plotter
 plotter = SunpyPlotter()
 # Plot a map
@@ -29,6 +30,7 @@ plotter.plot_solar_axis()
 # We can also plot an arbitrary point by passing a single coordinate to
 # :meth:`~sunkit_pyvista.plotter.SunpyPlotter.plot_coordinates`.
 # Plotting a point on the brightest pixel in the map
+
 pixel_pos = np.argwhere(low_res_aia_193.data == low_res_aia_193.data.max()) * u.pixel
 hpc_max = low_res_aia_193.pixel_to_world(pixel_pos[:, 1], pixel_pos[:, 0])
 plotter.plot_coordinates(hpc_max, color='blue')
@@ -39,10 +41,10 @@ bottom_left = SkyCoord(30*u.deg, -10*u.deg,
                        obstime=low_res_aia_193.date)
 plotter.plot_quadrangle(bottom_left=bottom_left, width=20*u.deg,
                         height=60*u.deg, color='blue')
-
 # Set the camera coordinate to view the plot correctly
 camera_coord = SkyCoord(30*u.deg, -10*u.deg, 6*R_sun,
                         frame=frames.HeliographicStonyhurst,
                         obstime=low_res_aia_193.date)
 plotter.set_camera_coordinate(camera_coord)
+
 plotter.show()
--- examples/assume_spherical.py
+++ examples/assume_spherical.py
@@ -16,7 +16,15 @@ import astropy.units as u
 from sunkit_pyvista import SunpyPlotter
 from sunkit_pyvista.sample import low_res_aia_193
 
+###############################################################################
+# We will use an AIA 193 image from the sunpy sample data as the base image.
+
+# Start by creating a plotter
 plotter = SunpyPlotter()
-plotter.plot_map(low_res_aia_193, clip_interval=[1, 99] * u.percent,
-                 assume_spherical_screen=False)
+# Plot a map setting the `assume_spherical_screen` to False
+plotter.plot_map(
+    low_res_aia_193, clip_interval=[1, 99] * u.percent,
+    assume_spherical_screen=False
+)
+
 plotter.show()

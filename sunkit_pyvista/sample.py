86d2fccc65e130973d20af47b246a19d033cec87
Nabil Freij <nabil.freij@gmail.com>
2022-06-20 13:57:03 -0700

    infra

--- sunkit_pyvista/sample.py
+++ sunkit_pyvista/sample.py
@@ -1,7 +1,7 @@
 import astropy.units as u
+import sunpy.data.sample
 import sunpy.map
-from sunpy.data.sample import AIA_193_IMAGE
 
 
 def low_res_aia_193(resolution=[512, 512] * u.pix):
-    return sunpy.map.Map(AIA_193_IMAGE).resample(resolution)
+    return sunpy.map.Map(sunpy.data.sample.AIA_193_IMAGE).resample(resolution)

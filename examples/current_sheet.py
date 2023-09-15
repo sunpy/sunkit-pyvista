89c9ae916c464cf9adcfa8ce92dde187851355c7
wuziqi <wuziqi@pku.edu.cn>
2023-09-15 22:46:02 +0800

    Add feature "plot current sheet"

--- examples/current_sheet.py
+++ examples/current_sheet.py
@@ -1,9 +1,9 @@
 """
 ================================
-Plotting Field Lines from pfsspy
+Plotting Current Sheets from pfsspy
 ================================
 
-``sunkit-pyvista`` can be used to plot field lines from ``pfsspy``.
+``sunkit-pyvista`` can be used to extract and plot the current sheet from ``pfsspy``.
 
 This example requires the pre-compiled FORTRAN
 code from `streamtracer <https://streamtracer.readthedocs.io/en/stable/>`__.
@@ -16,26 +16,13 @@ import pfsspy
 import sunpy.map
 from astropy.constants import R_sun
 from astropy.coordinates import SkyCoord
+
 from matplotlib import colors
 from pfsspy import tracing
 from pfsspy.sample_data import get_gong_map
 from sunpy.coordinates import frames
 
 from sunkit_pyvista import SunpyPlotter
-from sunkit_pyvista.sample import LOW_RES_AIA_193
-
-###############################################################################
-# We will be using an AIA 193 image from the sunpy sample data as the base image.
-
-# sphinx_gallery_defer_figures
-
-# Start by creating a plotter
-plotter = SunpyPlotter()
-
-# Plot a map
-plotter.plot_map(LOW_RES_AIA_193, clip_interval=[1, 99] * u.percent)
-# Add an arrow to show the solar rotation axis
-plotter.plot_solar_axis()
 
 ###############################################################################
 # We now need to do the magnetic field extrapolation using ``pfsspy``.
@@ -45,13 +32,16 @@ plotter.plot_solar_axis()
 # We load a gong_map from pfsspy
 gong_fname = get_gong_map()
 gong_map = sunpy.map.Map(gong_fname)
+gong_map.plot()
+
+plotter = SunpyPlotter()
 # Now we plot the Gong Map to fill in the farside.
 plotter.plot_map(gong_map, cmap="hmimag", clip_interval=[1, 99] * u.percent)
 
 # Create points spaced between lat={-90, 90} degrees
-lat = np.linspace(-np.pi / 2, np.pi / 2, 32, endpoint=False)
+lat = np.linspace(-np.pi / 2, np.pi / 2, 16, endpoint=False)
 # Create 32 points spaced between long={0, 360} degrees
-lon = np.linspace(0, 2 * np.pi, 32, endpoint=False)
+lon = np.linspace(0, np.pi*2, 16, endpoint=False)
 # Make a 2D grid from these 1D points
 lat, lon = np.meshgrid(lat, lon, indexing="ij")
 # Create lon, lat and radial coordinate values by using a pfsspy
@@ -59,11 +49,11 @@ lat, lon = np.meshgrid(lat, lon, indexing="ij")
 lat, lon = lat.ravel() * u.rad, lon.ravel() * u.rad
 # Define the number of grid points in rho and solar surface radius
 nrho = 30
-rss = 1.5
+rss = 2.5
 input_ = pfsspy.Input(gong_map, nrho, rss)
 output_ = pfsspy.pfss(input_)
-seeds = SkyCoord(lon, lat, 1.2 * R_sun, frame=gong_map.coordinate_frame)
-tracer = tracing.FortranTracer()
+seeds = SkyCoord(lon, lat, 2.4 * R_sun, frame=gong_map.coordinate_frame)
+tracer = tracing.PythonTracer()
 field_lines = tracer.trace(seeds, output_)
 
 ###############################################################################
@@ -81,15 +71,7 @@ def my_fline_color_func(field_line):
 
 # Plotting the field lines
 plotter.plot_field_lines(field_lines, color_func=my_fline_color_func)
-
-# Set the camera coordinate to view the plot correctly
-camera_coord = SkyCoord(
-    0 * u.deg,
-    0 * u.deg,
-    6 * R_sun,
-    frame=frames.HeliographicStonyhurst,
-    obstime=LOW_RES_AIA_193.date,
-)
-plotter.set_camera_coordinate(camera_coord)
+# Plotting the current sheet
+plotter.plot_current_sheet(output_)
 
 plotter.show()

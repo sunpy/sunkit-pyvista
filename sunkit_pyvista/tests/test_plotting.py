f91f1f9e06ddbb8ed78e58959c3b92bc03ef95a2
wuziqi <wuziqi@pku.edu.cn>
2023-10-03 20:36:34 +0800

    add test current sheet figure. But fail to run any tests in the folder. message: "_jb_pytest_runner.py: error: unrecognized arguments: --doctest-rst"

--- sunkit_pyvista/tests/test_plotting.py
+++ sunkit_pyvista/tests/test_plotting.py
@@ -72,3 +72,39 @@ def test_plot_map_with_functionality(
     plotter.load(filepath)
 
     plotter.show(cpos=(0, 1, 0), before_close_callback=verify_cache_image)
+
+
+def test_field_lines_figure(aia171_test_map, plotter, verify_cache_image):
+    gong_fname = get_gong_map()
+    gong_map = smap.Map(gong_fname)
+    nrho = 35
+    rss = 2.5
+    lat = np.linspace(-np.pi / 2, np.pi / 2, 8, endpoint=False)
+    lon = np.linspace(0, 2 * np.pi, 8, endpoint=False)
+    lat, lon = np.meshgrid(lat, lon, indexing="ij")
+    lat, lon = lat.ravel() * u.rad, lon.ravel() * u.rad
+    radius = 1.2
+    tracer = tracing.PythonTracer()
+    input_ = pfsspy.Input(gong_map, nrho, rss)
+    output_ = pfsspy.pfss(input_)
+    seeds = SkyCoord(lon, lat, radius * const.R_sun, frame=gong_map.coordinate_frame)
+    field_lines = tracer.trace(seeds, output_)
+
+    def color_function(field_line):
+        norm = colors.LogNorm(vmin=1, vmax=1000)
+        cmap = plt.get_cmap("magma")
+        return cmap(norm(np.abs(field_line.expansion_factor)))
+
+    plotter.plot_map(aia171_test_map)
+    plotter.plot_field_lines(field_lines, color_func=color_function)
+    plotter.show(cpos=(0, 1, 0), before_close_callback=verify_cache_image)
+
+def test_current_sheet_figure(plotter,verify_cache_image):
+    gong_fname = get_gong_map()
+    gong_map = smap.Map(gong_fname)
+    nrho = 35
+    rss = 2.5
+    input_ = pfsspy.Input(gong_map, nrho, rss)
+    output_ = pfsspy.pfss(input_)
+    plotter.plot_current_sheet(output_)
+    plotter.show(before_close_callback=verify_cache_image)

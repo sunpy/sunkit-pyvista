4f9fd5dd82adcaa76f59479e0e54402f601270a3
Nabil Freij <nabil.freij@gmail.com>
2025-06-20 11:07:03 +0100

    increase min: magex

--- sunkit_pyvista/sample.py
+++ sunkit_pyvista/sample.py
@@ -1,4 +1,4 @@
-import warning
+import warnings
 
 import astropy.units as u
 
@@ -7,10 +7,10 @@ import sunpy.map
 __all__ = ["LOW_RES_AIA_193"]
 
 
-with warning.catch_warnings():
+with warnings.catch_warnings():
     # Ignore the warning about the FITS file not being in the correct format
     # This is because the FITS file is a low-resolution version of an AIA image
-    warning.simplefilter("ignore")
+    warnings.simplefilter("ignore")
     LOW_RES_AIA_193 = sunpy.map.Map(
         "https://github.com/sunpy/data/raw/main/sunpy/v1/AIA20110607_063307_0193_lowres.fits",
     )

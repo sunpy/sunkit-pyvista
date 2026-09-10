1a5b16de8f27b7fb7515e9e35f50f3a6752a822e
Shane Maloney <maloneys@tcd.ie>
2026-09-10 13:49:40 +0100

    Remove Xvfb as recent VTK releases no longer require it (#244)
    Co-authored-by: Stuart Mumford <stuart@cadair.com>

--- sunkit_pyvista/sample.py
+++ sunkit_pyvista/sample.py
@@ -1,17 +1,13 @@
 import warnings
 
 import astropy.units as u
+from astropy.io import fits
 
 import sunpy.map
+from sunpy.data.sample import AIA_193_IMAGE
 
 __all__ = ["LOW_RES_AIA_193"]
 
-
 with warnings.catch_warnings():
-    # Ignore the warning about the FITS file not being in the correct format
-    # This is because the FITS file is a low-resolution version of an AIA image
-    warnings.simplefilter("ignore")
-    LOW_RES_AIA_193 = sunpy.map.Map(
-        "https://github.com/sunpy/data/raw/main/sunpy/v1/AIA20110607_063307_0193_lowres.fits",
-    )
-    LOW_RES_AIA_193 = LOW_RES_AIA_193.resample([512, 512] * u.pix, method="spline")
+    warnings.simplefilter("ignore", fits.verify.VerifyWarning)
+    LOW_RES_AIA_193 = sunpy.map.Map(AIA_193_IMAGE).resample([512, 512] * u.pix, method="spline")

26d9a312b57cb35ad2b809f293faa0fa467a6907
Stuart Mumford <stuart@cadair.com>
2026-09-10 09:43:42 +0100

    Fix docs and import sunpy sample data

--- sunkit_pyvista/sample.py
+++ sunkit_pyvista/sample.py
@@ -1,9 +1,13 @@
+import warnings
+
 import astropy.units as u
+from astropy.io import fits
 
 import sunpy.map
 from sunpy.data.sample import AIA_193_IMAGE
 
 __all__ = ["LOW_RES_AIA_193"]
 
-
-LOW_RES_AIA_193 = sunpy.map.Map(AIA_193_IMAGE).resample([512, 512] * u.pix, method="spline")
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", fits.verify.VerifyWarning)
+    LOW_RES_AIA_193 = sunpy.map.Map(AIA_193_IMAGE).resample([512, 512] * u.pix, method="spline")
